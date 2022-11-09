58ba3432734a62105555d157047bf5e75112cd58
Jakub Kozera <jakub.kozera@inovirtue.com>
2022-11-09 18:55:14 +0100

    Add project files.

---
+++ Strings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+var life = "brutal";
+var stringLiteral = $$""""
+    tes
+    test
+    {
+        "name": "FirstName",
+        "label": "First Name",
+        "type": "Text",
+        "required": true
+    }
+    """Life is {{life}}"""
+    """";
+
+Console.WriteLine(stringLiteral);
+
+var rating = 3;
+
+var message = $"This course has rating of {rating}, which means its {rating switch
+{
+    < 3 => "sucks",
+    _ => "not bad"
+}}";
+
+Console.WriteLine(message);
+
+var oldEncoding = Encoding.UTF8.GetBytes("Some value");
+
+var newEncoding = "Some value"u8.ToArray();
