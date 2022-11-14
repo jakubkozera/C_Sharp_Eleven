a667dff0ca0ad462894cb3695f5af004f39ffeba
Jakub Kozera <jakub.kozera@inovirtue.com>
2022-11-14 07:33:21 +0100

    base1

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -2,7 +2,7 @@
 using System.Text;
 
 
-string filename1 = @"c:\documents\files\filename.txt";
+string filename1 = @"c:\documents\files\.txt";
 string filename2 = "c:\\documents\\files\\filename.txt";
 
 Console.WriteLine(filename1);
